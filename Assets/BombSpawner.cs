3a6d8caab6ccc3d665dc95d6b9abb7c07d6870a8
Yasir Haq <yasir.haq98@gmail.com>
2018-09-27 13:36:51 +0700

    Bomberman Clone

---
+++ Assets/BombSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour {
+
+    public Tilemap tilemap;
+    public GameObject bombPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bombSpawner();
+    }
+
+    private void bombSpawner() {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            Vector3Int cell = tilemap.WorldToCell(transform.position);
+            Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        }
+    }
+}
